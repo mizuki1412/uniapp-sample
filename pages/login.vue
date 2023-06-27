cbf964a05768c9a1d4d4a2b8c1735778ac6a4cc2
ycj <yingchaojie@163.com>
2023-06-27 12:29:01 +0800

    add login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,16 @@
 <template>
-	<view class="_flex_col_center">
-		<image class="w-24 h-24 mt-4" src="/static/logo.png"></image>
-		<view class="mt-2">
-			<text class="text-green-600">{{title}}</text>
+	<view class="_flex_col_center h-screen w-screen bg-green-700 text-white">
+		<view class="text-2xl mb-4">登录</view>
+		<view class="w-[80%] h-[400rpx] bg-gray-100 shadow rounded p-3 flex flex-col justify-center">
+			<uni-forms ref="baseForm" :rules="rules" :modelValue="baseFormData">
+				<uni-forms-item label="用户名" required name="name">
+					<uni-easyinput v-model="baseFormData.name" placeholder="请输入姓名" />
+				</uni-forms-item>
+				<uni-forms-item label="密码" required name="pwd">
+					<uni-easyinput v-model="baseFormData.pwd" placeholder="请输入密码" />
+				</uni-forms-item>
+			</uni-forms>
+			<button type="primary" @click="submit()" class="w-full">登录</button>
 		</view>
 	</view>
 </template>
@@ -10,9 +18,31 @@
 <script setup>
   import {ref} from 'vue'
   import {onLoad} from '@dcloudio/uni-app'
+	import {route} from "../lib/service/route";
+	import {config} from "../lib/store/config";
 
   const title = ref('login')
-	onLoad((e)=>{
-    console.log("load", e)
-  })
+  const baseForm = ref()
+	const baseFormData = ref({})
+	const rules = ref({
+		name: {
+			rules: [{
+				required: true,
+				errorMessage: '姓名不能为空'
+			}]
+		},
+		pwd: {
+			rules: [{
+				required: true,
+				errorMessage: '密码不能为空'
+			}]
+		}
+	})
+
+	async function submit(){
+		await baseForm.value.validate()
+		console.log(baseFormData.value)
+		// updateStoreUserInfo()
+		route('pages/home',null,{type:'switchTab'})
+	}
 </script>
